efff4a3b37b8527b8c8a5e533c24969aa2383eb3
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-06-03 15:47:24 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,4 @@
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -5,20 +6,32 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+
   const delay = Number(form.elements.delay.value);
   const state = form.elements.state.value;
 
-  setTimeout(() => {
-    if (state === 'fulfilled') {
+
+  createDelayedPromise(delay, state)
+    .then(message => {
       iziToast.success({
         title: '✅ Success',
-        message: `Promise resolved in ${delay}ms`,
+        message,
       });
-    } else {
+    })
+    .catch(message => {
       iziToast.error({
         title: '❌ Error',
-        message: `Promise rejected in ${delay}ms`,
+        message,
       });
-    }
-  }, delay);
+    });
 });
+
+
+function createDelayedPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const msg = `Promise ${state === 'fulfilled' ? 'resolved' : 'rejected'} in ${delay}ms`;
+      state === 'fulfilled' ? resolve(msg) : reject(msg);
+    }, delay);
+  });
+}
